057b072c2bdaa9db91e5c47dce0559e4df9af5b4
Margie Metz <84488543+metzmom@users.noreply.github.com>
2021-06-10 22:07:01 +0000

    inital start of assignment.  const input = require(); let num1 = input.question("Enter a number: "); console.log (Number(num1)) not working

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 //Part A #1: Initialize variables to store the following arrays. Remember to use descriptive names.
+const input = require(`readline-sync`);
+
+let num1 = input.question("Enter a number: ");
+console.log (Number(num1))
+
+
+
+
+
+/*
 
 let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
 let grain = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
@@ -7,9 +17,19 @@ let beverage = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
 let dessert = ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi'];
 
 //Part A #2: Construct a for loop that assembles a meal for each of 6 astronauts.
+//const input = require(`readline-sync`);
+//const input = require(`readline-sync`);
+
+//let mealCount = input.question("Enter a number: ");
+
+//let mealCount = input.question("Enter number of meals to assemble: ");
+
+
 
 for (i=0; i<6;i++) {
-  console.log("Meal plan option"[i])
+  console.log("Meal plan option",(i+1));
+  console.log(protein[i],grain[i], vegetable[i],beverage[i],dessert[i]);
+
 }
 
 //Part C #5: Using a while loop, ask the user to select the number of meals to assemble.  Validate the input to make sure it is an integer from 1 - 6.
@@ -18,4 +38,4 @@ for (i=0; i<6;i++) {
 //Skill boosts: 
 // a. Use string formatting to print something more interesting than “[‘chicken’, ‘rice’, ‘peas’, ‘juice’, ‘apple’]” for the meal outputs.
 // b. Use an “array of arrays” to store the food options in a ‘pantry’.
-// c. Modify your code to check each meal for kale.  If present, after the meal output add, “Don’t worry, you can have double chocolate tomorrow.”
+// c. Modify your code to check each meal for kale.  If present, after the meal output add, “Don’t worry, you can have double chocolate tomorrow.”*/
